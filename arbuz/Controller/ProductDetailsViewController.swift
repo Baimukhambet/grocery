4f4502949117edbf1c3aa9696deb2b1a2d015c58
Timka <vendetta10090@gmail.com>
2024-05-21 13:49:10 +0500

    cart functionality inside details screen is done

--- arbuz/Controller/ProductDetailsViewController.swift
+++ arbuz/Controller/ProductDetailsViewController.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Combine
 
 class ProductDetailsViewController: UIViewController {
     let productService: ProductService = ProductService.shared
@@ -9,6 +10,48 @@ class ProductDetailsViewController: UIViewController {
     
     
     //MARK: Subviews
+    lazy var topBarView: UIView = {
+        let closeButton = UIButton()
+        let likeButton = UIButton()
+        
+        closeButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+
+        closeButton.imageView?.contentMode = .scaleAspectFit
+        likeButton.imageView?.contentMode = .scaleAspectFit
+        
+        closeButton.tintColor = .black
+        likeButton.tintColor = .black
+        
+        closeButton.addAction(UIAction{ _ in self.dismiss(animated: true)}, for: .touchUpInside)
+        
+        let topBarView = UIView()
+        closeButton.translatesAutoresizingMaskIntoConstraints = false
+        likeButton.translatesAutoresizingMaskIntoConstraints = false
+        topBarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        topBarView.addSubview(closeButton)
+        topBarView.addSubview(likeButton)
+        
+        NSLayoutConstraint.activate([
+            likeButton.widthAnchor.constraint(equalToConstant: 40),
+            likeButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            closeButton.widthAnchor.constraint(equalToConstant: 40),
+            closeButton.heightAnchor.constraint(equalToConstant: 40),
+            
+            closeButton.leadingAnchor.constraint(equalTo: topBarView.leadingAnchor, constant: 8),
+            closeButton.topAnchor.constraint(equalTo: topBarView.topAnchor, constant: 8),
+            closeButton.bottomAnchor.constraint(equalTo: topBarView.bottomAnchor, constant: -8),
+            
+            likeButton.trailingAnchor.constraint(equalTo: topBarView.trailingAnchor, constant: -8),
+            likeButton.topAnchor.constraint(equalTo: topBarView.topAnchor, constant: 8),
+            likeButton.bottomAnchor.constraint(equalTo: topBarView.bottomAnchor, constant: -8),
+        ])
+        
+        return topBarView
+    }()
+    
     lazy var productImageView: UIImageView = {
         let imageView = UIImageView()
         imageView.translatesAutoresizingMaskIntoConstraints = false
@@ -17,6 +60,50 @@ class ProductDetailsViewController: UIViewController {
         
         return imageView
     }()
+    
+    lazy var imageBackgroundView: UIView = {
+        let view = UIView()
+        view.backgroundColor = COLOR.secondary
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        view.layer.cornerRadius = 16
+        
+        return view
+    }()
+    
+    lazy var productTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: FONTSIZE.titleLarge, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    lazy var buyButton: BuyButton = {
+        let price = cartVM.inCart(product: product!) ? cartVM.cart[self.product!]! * self.product!.price : product!.price
+        let qty = cartVM.inCart(product: product!) ? "\(cartVM.cart[self.product!]!)" : nil
+        
+        let btn = BuyButton(priceString: "\(price)", inCart: cartVM.inCart(product: product!), qtyString: qty)
+        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.onMinus = {
+            self.cartVM.decrement(product: self.product!)
+            if self.cartVM.inCart(product: self.product!){
+                let newPrice = self.cartVM.cart[self.product!]! * self.product!.price
+                btn.setPriceLabel(price: "\(newPrice) ₸")
+                let qty = self.cartVM.cart[self.product!]!
+                btn.setQtyLabel(qty: "\(qty)")
+            }
+        }
+        btn.onPlus = {
+            self.cartVM.addToCart(product: self.product!)
+            let newPrice = self.cartVM.cart[self.product!]! * self.product!.price
+            btn.setPriceLabel(price: "\(newPrice) ₸")
+            let qty = self.cartVM.cart[self.product!]!
+            btn.setQtyLabel(qty: "\(qty)")
+        }
+        
+        return btn
+    }()
+    
     
     init(id: String) {
         self.id = id
@@ -30,19 +117,106 @@ class ProductDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        
+        setupView()
         loadProduct()
+        
+        
     }
+    
+    private var cancellables = Set<AnyCancellable>()
 }
 
 
 //MARK: Private functions
 private extension ProductDetailsViewController {
+    func setupView() {
+        view.addSubview(topBarView)
+        view.addSubview(imageBackgroundView)
+        imageBackgroundView.addSubview(productImageView)
+        NSLayoutConstraint.activate([
+            topBarView.topAnchor.constraint(equalTo: view.topAnchor),
+            topBarView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            topBarView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            
+            imageBackgroundView.topAnchor.constraint(equalTo: topBarView.bottomAnchor),
+            imageBackgroundView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            imageBackgroundView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            imageBackgroundView.heightAnchor.constraint(equalToConstant: UIApplication.windowSize!.height * 0.5),
+            
+            productImageView.topAnchor.constraint(equalTo: imageBackgroundView.topAnchor),
+            productImageView.leadingAnchor.constraint(equalTo: imageBackgroundView.leadingAnchor, constant: 16),
+            productImageView.trailingAnchor.constraint(equalTo: imageBackgroundView.trailingAnchor, constant: -16),
+            productImageView.bottomAnchor.constraint(equalTo: imageBackgroundView.bottomAnchor),
+        ])
+        
+        setupTitleLabel()
+    }
+    
+    func setupTitleLabel() {
+        view.addSubview(productTitleLabel)
+        NSLayoutConstraint.activate([
+            productTitleLabel.topAnchor.constraint(equalTo: imageBackgroundView.bottomAnchor, constant: 16),
+            productTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            productTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+        ])
+    }
+    
+    
     func loadProduct() {
         productService.fetchProduct(id: id) { product in
             if let product = product {
+                self.product = product
+                DispatchQueue.main.async {
+                    self.productTitleLabel.text = product.strIngredient
+                    self.setupButton()
+                    self.subscribe()
+                }
                 
+                self.fetchImage()
             }
         }
     }
+    
+    func fetchImage() {
+        productService.loadImage(urlString: self.product!.imageUrl) { image in
+            DispatchQueue.main.async {
+                guard let image = image else {
+                    self.setMockImage()
+                    return
+                }
+                self.productImageView.image = image
+            }
+        }
+    }
+    
+    func setMockImage() {
+        guard let offlineImage = UIImage(named: product!.strIngredient!) else {
+            productImageView.image = UIImage(systemName: "photo")
+            return
+        }
+        productImageView.image = offlineImage
+    }
+    
+    func setupButton() {
+        self.view.addSubview(buyButton)
+        NSLayoutConstraint.activate([
+            buyButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            buyButton.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            buyButton.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            buyButton.heightAnchor.constraint(equalToConstant: 90)
+        ])
+    }
+    
+    func subscribe() {
+        cartVM.$cart.receive(on: RunLoop.main).sink { [self] _ in
+            if(cartVM.inCart(product: product!)) {
+                buyButton.addedToCart()
+            } else {
+                buyButton.removeFromCart()
+            }
+            
+        }
+        .store(in: &cancellables)
+    }
+    
 }
